fce7aa0b9ea50dde70f745fb1d1194e6777cba76
Leonardo Valença <lpvalenca@hotmail.com>
2022-09-05 15:37:36 -0300

    Project created + ios/android versions created using Capacitor

---
+++ revoei/android/app/src/main/java/com/lpvlnc/revoei/MainActivity.java
@@ -0,0 +1,5 @@
+package com.lpvlnc.revoei;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ revoei/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lpvlnc.revoei',
+  appName: 'revoei',
+  webDir: 'dist/revoei',
+  bundledWebRuntime: false
+};
+
+export default config;
